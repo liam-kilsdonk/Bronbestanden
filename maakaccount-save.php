8c2c5ad856d3c767732e584cf3e676e4a7a5c0ba
1197777 <1197777@student.roc-nijmegen.nl>
2024-01-25 12:49:18 +0100

    -

--- maakaccount-save.php
+++ maakaccount-save.php
@@ -1,45 +1,46 @@
 <?php
-     include_once("functions.php");
+include_once("functions.php");
 
-     $db = ConnectDB();
-     
-     $naam = $_GET['Naam'];
-     $email = $_GET['Email'];
-     $telefoon = $_GET['Telefoon'];
-     $wachtwoord = $_GET['Wachtwoord'];
-     
-     $sql = "INSERT INTO relaties (Naam, Email, Telefoon, Wachtwoord, FKrollenID)
-                  VALUES ('" . $naam . "', '" . 
-                               $email . "', '" .
-                               $telefoon . "', '" . 
-                               md5($wachtwoord) . "', '" . 
-                               10 . "')";
-     
-     if ($db->query($sql) == true) 
-     {    if (StuurMail($email, 
-                        "Account gegevens Ultima Casa", 
-                        "Uw inlog gegevens zijn:
-                        
-               Naam: " . $naam . "
-               E-mailadres: " . $email . "
-               Telefoon: " . $telefoon . "
-               Wachtwoord: " . $wachtwoord . "
-               
-               Bewaar deze gegevens goed!
-               
-               Met vriendelijke groet,
-               
-               Het Ultima Casa team.",
-                        "From: noreply@uc.nl"))
-          {    $result = 'De gegevens zijn naar uw e-mail adres verstuurd.';
-          }
-          else
-          {    $result = 'Fout bij het versturen van de e-mail met uw gegevens.';
-          }
-     }
-     else
-     {    $result .= 'Fout bij het bewaren van uw gegevens.<br><br>' . $sql;
-     }
-     echo $result . '<br><br>
-          <button class="action-button"><a href="index.html">Ok</a></button>';
+$db = ConnectDB();
+
+$naam = isset($_GET['Naam']) ? $_GET['Naam'] : "";
+$email = isset($_GET['Email']) ? $_GET['Email'] : "";
+$telefoon = isset($_GET['Telefoon']) ? $_GET['Telefoon'] : "";
+$wachtwoord = isset($_GET['Wachtwoord']) ? $_GET['Wachtwoord'] : "";
+
+$sql = "INSERT INTO relaties (Naam, Email, Telefoon, Wachtwoord, FKrollenID)
+        VALUES (:naam, :email, :telefoon, :wachtwoord, :fkrollenid)";
+
+$stmt = $db->prepare($sql);
+
+$md5wachtwoord = md5($wachtwoord);
+
+$stmt->bindParam(':naam', $naam, PDO::PARAM_STR);
+$stmt->bindParam(':email', $email, PDO::PARAM_STR);
+$stmt->bindParam(':telefoon', $telefoon, PDO::PARAM_STR);
+
+$stmt->bindParam(':wachtwoord', $md5wachtwoord, PDO::PARAM_STR);
+
+$stmt->bindParam(':fkrollenid', 10, PDO::PARAM_INT);
+
+if ($stmt->execute()) {
+    if (StuurMail($email, "Account gegevens Ultima Casa", "Uw inlog gegevens zijn:\n\n
+                   Naam: $naam\n
+                   E-mailadres: $email\n
+                   Telefoon: $telefoon\n
+                   Wachtwoord: $wachtwoord\n\n
+                   Bewaar deze gegevens goed!\n\n
+                   Met vriendelijke groet,\n\n
+                   Het Ultima Casa team.",
+        "From: noreply@uc.nl")) {
+        $result = 'De gegevens zijn naar uw e-mailadres verstuurd.';
+    } else {
+        $result = 'Fout bij het versturen van de e-mail met uw gegevens.';
+    }
+} else {
+    $result = 'Fout bij het bewaren van uw gegevens.<br><br>' . print_r($stmt->errorInfo(), true);
+}
+
+echo $result . '<br><br>
+      <button class="action-button"><a href="index.html">Ok</a></button>';
 ?>
